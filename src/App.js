5994f3ca79c75c5b8ad08b1f720a1d006f2f34f5
Apoorv <tofu.develops@gmail.com>
2021-09-30 22:55:21 +0530

    Added anime chan api (#5)
    * added anime-chan api
    
    * um last changes
    
    Co-authored-by: ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Message from "./components/content";
 
 import RandomQuotes from "./cards/randomQuotes";
 import RandomAdvice from "./cards/randomAdvice";
+
+import RandomAnime from "./cards/randomAnime";
 import StarWarsAPI from "./cards/starWarsAPI";
+
 
 
 function App() {
@@ -12,10 +15,12 @@ function App() {
     <div className="mx-0 xl:mx-40 font-body">
       <Header />
       <Message />
-
+    
       <RandomQuotes />
       <RandomAdvice />
+      <RandomAnime/>
       <StarWarsAPI />
+
     </div>
   );
 }
