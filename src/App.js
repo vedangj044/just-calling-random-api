676edd95072827a03541898ba1585d3b109ff3e9
Shivam Singh <shivm.0701@gmail.com>
2021-10-02 01:53:10 +0530

    added random github user api

--- src/App.js
+++ src/App.js
@@ -17,31 +17,33 @@ import BreadPuns from "./cards/breadPuns";
 import PokeApi from "./cards/pokeAPI";
 import RandomCreditCard from "./cards/randomCreditCard";
 import RandomFoxImage from "./cards/randomFoxImage";
+import GitHubUser from "./cards/githubUser";
 
 function App() {
-  return (
-    <div className="mx-0 xl:mx-40 font-body">
-      <Header />
-      <Message />
-      <RandomAnimfacts />
-      <MusicPlayer />
-      <RandomQuotes />
-      <RandomAdvice />
-      <RandomAnime />
-      <StarWarsAPI />
-      <RandomDadJoke />
-      <RandomCatFacts />
-      <ChuckNorrisJoke />
-      <RandomGeekJokes />
-      <RandomBriyaniPics />
-      <RandomGeekJokes />
-      <RandomActivity />
-      <BreadPuns />
-      <PokeApi />
-      <RandomCreditCard />
-      <RandomFoxImage />
-    </div>
-  );
+    return (
+        <div className="mx-0 xl:mx-40 font-body">
+            <Header />
+            <Message />
+            <RandomAnimfacts />
+            <MusicPlayer />
+            <RandomQuotes />
+            <RandomAdvice />
+            <RandomAnime />
+            <StarWarsAPI />
+            <RandomDadJoke />
+            <RandomCatFacts />
+            <ChuckNorrisJoke />
+            <RandomGeekJokes />
+            <RandomBriyaniPics />
+            <RandomGeekJokes />
+            <RandomActivity />
+            <BreadPuns />
+            <PokeApi />
+            <RandomCreditCard />
+            <RandomFoxImage />
+            <GitHubUser />
+        </div>
+    );
 }
 
 export default App;
