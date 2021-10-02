7ba032862252f7dd7c1ad86c23b969a714be539b
Anish Mittal <52633729+ANISH0309@users.noreply.github.com>
2021-10-02 11:33:42 +0530

    add kanye.rest api (#29)

---
+++ src/cards/RandomKanyeQuote.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import ContributorInfo from "../components/contributorInfo";
+import ApiInfo from "../components/apiInfo";
+
+export default function RandomKanyeQuote(props) {
+	const [kanyeQuote, setKanyeQuote] = useState("");
+
+	useEffect(() => {
+		fetch("https://api.kanye.rest")
+			.then((res) => res.json())
+			.then((data) => setKanyeQuote(data.quote));
+	}, []);
+	return (
+		<div>
+			<div className="mx-4 my-8 rounded-md p-4 border-2 border-grey-300 md:px-4">
+				<h1 className="text-xl py-2">Random Kanye West Quotes</h1>
+				<h1 className="text-2xl font-medium pb-6">{kanyeQuote}</h1>
+				<ContributorInfo
+					username="ANISH0309"
+					profilePic="https://avatars.githubusercontent.com/ANISH0309"
+				/>
+				<ApiInfo apiUrl="https://kanye.rest/" apiName="Kanye.rest" />
+			</div>
+		</div>
+	);
+}
