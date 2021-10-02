688bca973d9d3ed5a7dc1d2e805f4d137b1bbef1
vedangj044 <vedangj044@gmail.com>
2021-10-02 10:28:52 +0530

    Fixed start here hyperlink

--- src/App.js
+++ src/App.js
@@ -35,7 +35,6 @@ function App() {
       <RandomDadJoke />
       <RandomCatFacts />
       <ChuckNorrisJoke />
-      <RandomGeekJokes />
       <RandomBriyaniPics />
       <RandomGeekJokes />
       <RandomActivity />
