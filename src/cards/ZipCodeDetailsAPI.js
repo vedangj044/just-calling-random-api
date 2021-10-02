40f0296880964283755e49f89fb255cdcd275e77
mdkacer <mdkacer123@gmail.com>
2021-10-02 16:58:06 +0530

    set zip code data to h1

--- src/cards/ZipCodeDetailsAPI.js
+++ src/cards/ZipCodeDetailsAPI.js
@@ -15,6 +15,11 @@ const ZipCodeAPI = (props) => {
 
     return (
         <div className="mx-4 my-8 rounded-md p-4 border-2 border-grey-300 md:px-4">
+            <h1 className="text-xl py-2">Zip Code Details</h1>
+            <h1 className="text-2xl font-medium pb-6">Zip Code : {zipCodeData["post code"]}</h1>
+            <h1 className="text-2xl font-medium pb-6">Total Deaths : {zipCodeData.country}</h1>
+            <h1 className="text-2xl font-medium pb-6">New Deaths : {zipCodeData["country abbreviation"]}</h1>
+
             
         </div>
     )
