a1fd84a8d8a4a28d52a920a829ff1e935d98d35d
Armakuji <armakuji@hotmail.com>
2021-10-03 22:40:46 +0700

    feat: add Random Movie Quote section

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import './App.css'
-import Header from './components/header'
-import Message from './components/content'
-import RandomKanyeQuote from './cards/RandomKanyeQuote'
-import MusicPlayer from './cards/MusicPlayer'
-import RandomQuotes from './cards/randomQuotes'
-import RandomAdvice from './cards/randomAdvice'
-import RandomAnime from './cards/randomAnime'
-import StarWarsAPI from './cards/starWarsAPI'
-import RandomDadJoke from './cards/randomDadJoke'
-import RandomCatFacts from './cards/randomCatFacts'
-import ChuckNorrisJoke from './cards/chuckNorrisJoke'
-import RandomGeekJokes from './cards/randomGeekJokes'
-import RandomBriyaniPics from './cards/RandomBriyaniPics'
-import RandomActivity from './cards/boredAPI'
-import RandomAnimfacts from './cards/randomDogfacts'
-import BreadPuns from './cards/breadPuns'
-import RandomDogimage from './cards/RandomDogimage'
-import PokeApi from './cards/pokeAPI'
-import RandomCreditCard from './cards/randomCreditCard'
-import RandomFoxImage from './cards/randomFoxImage'
-import Covidapi from './cards/covidApi'
-import GitHubUser from './cards/githubUser'
-import RandomCatimages from './cards/RandomCatimages'
-import ZipCodeDetailsAPI from './cards/ZipCodeDetailsAPI'
-import ImageCompression from './cards/imageCompressionAPI'
-import GiphyApi from './cards/giphyapi'
-import Dungeons from './cards/Dungeons'
-import WeatherApi from './cards/weatherApi';
-import UrlShortener from './cards/url-shortener'
-import CryptoDetails from './cards/cryptoDetails'
+import "./App.css";
+import Header from "./components/header";
+import Message from "./components/content";
+import RandomKanyeQuote from "./cards/RandomKanyeQuote";
+import MusicPlayer from "./cards/MusicPlayer";
+import RandomQuotes from "./cards/randomQuotes";
+import RandomAdvice from "./cards/randomAdvice";
+import RandomAnime from "./cards/randomAnime";
+import StarWarsAPI from "./cards/starWarsAPI";
+import RandomDadJoke from "./cards/randomDadJoke";
+import RandomCatFacts from "./cards/randomCatFacts";
+import ChuckNorrisJoke from "./cards/chuckNorrisJoke";
+import RandomGeekJokes from "./cards/randomGeekJokes";
+import RandomBriyaniPics from "./cards/RandomBriyaniPics";
+import RandomActivity from "./cards/boredAPI";
+import RandomAnimfacts from "./cards/randomDogfacts";
+import BreadPuns from "./cards/breadPuns";
+import RandomDogimage from "./cards/RandomDogimage";
+import PokeApi from "./cards/pokeAPI";
+import RandomCreditCard from "./cards/randomCreditCard";
+import RandomFoxImage from "./cards/randomFoxImage";
+import Covidapi from "./cards/covidApi";
+import GitHubUser from "./cards/githubUser";
+import RandomCatimages from "./cards/RandomCatimages";
+import ZipCodeDetailsAPI from "./cards/ZipCodeDetailsAPI";
+import ImageCompression from "./cards/imageCompressionAPI";
+import GiphyApi from "./cards/giphyapi";
+import Dungeons from "./cards/Dungeons";
+import WeatherApi from "./cards/weatherApi";
+import UrlShortener from "./cards/url-shortener";
+import CryptoDetails from "./cards/cryptoDetails";
 import RandomJokeApi from "./cards/randomJokeApi";
 import RicknMorty from "./cards/ricknmortyApi";
-
+import RandomMoiveQuote from "./cards/RandomMovieQuote";
 
 function App() {
   return (
@@ -68,6 +68,7 @@ function App() {
       <RicknMorty />
       <CryptoDetails />
       <RandomJokeApi />
+      <RandomMoiveQuote />
     </div>
   );
 }
