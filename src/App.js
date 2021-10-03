07fd79c5d97454fd16677c83e5b2d15685d20459
Dikshansh Kumar Lodhi <80641727+deekay1712@users.noreply.github.com>
2021-10-03 19:11:57 +0530

    Added Gotiny Api (#40)
    Co-authored-by: Vedang Joshi <43697446+vedangj044@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ import ImageCompression from './cards/imageCompressionAPI'
 import GiphyApi from './cards/giphyapi'
 import Dungeons from './cards/Dungeons'
 import WeatherApi from './cards/weatherApi';
+import UrlShortener from './cards/url-shortener'
 import CryptoDetails from './cards/cryptoDetails'
 import RandomJokeApi from "./cards/randomJokeApi";
 
@@ -42,6 +43,7 @@ function App() {
       <RandomKanyeQuote />
       <RandomAnimfacts />
       <MusicPlayer />
+      <UrlShortener />
       <RandomQuotes />
       <RandomAdvice />
       <RandomAnime />
