29fa332daf72ababac783e1eb64867cd17a3471b
Sejal Rai <sejalrai23@gmail.com>
2021-10-03 17:31:31 +0530

    added crypto details api (#38)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import './App.css'
 import Header from './components/header'
 import Message from './components/content'
@@ -27,40 +28,43 @@ import ImageCompression from './cards/imageCompressionAPI'
 import GiphyApi from './cards/giphyapi'
 import Dungeons from './cards/Dungeons'
 import WeatherApi from './cards/weatherApi';
+import CryptoDetails from './cards/cryptoDetails'
 
 function App() {
-  return (
-    <div className='mx-0 xl:mx-40 font-body'>
-      <Header />
-      <Message />
-      <Dungeons />
-      <GiphyApi />
-      <ImageCompression />
-      <RandomKanyeQuote />
-      <RandomAnimfacts />
-      <MusicPlayer />
-      <RandomQuotes />
-      <RandomAdvice />
-      <RandomAnime />
-      <StarWarsAPI />
-      <RandomDadJoke />
-      <RandomCatFacts />
-      <ChuckNorrisJoke />
-      <RandomBriyaniPics />
-      <RandomGeekJokes />
-      <RandomActivity />
-      <BreadPuns />
-      <RandomDogimage />
-      <PokeApi />
-      <RandomCreditCard />
-      <RandomFoxImage />
-      <GitHubUser />
-      <RandomCatimages />
-      <ZipCodeDetailsAPI />
-      <WeatherApi />
-      <Covidapi />
-    </div>
-  )
+    return (
+        <div className='mx-0 xl:mx-40 font-body'>
+            <Header />
+            <Message />
+            <Dungeons />
+            <GiphyApi />
+            <ImageCompression />
+            <RandomKanyeQuote />
+            <RandomAnimfacts />
+            <MusicPlayer />
+            <RandomQuotes />
+            <RandomAdvice />
+            <RandomAnime />
+            <StarWarsAPI />
+            <RandomDadJoke />
+            <RandomCatFacts />
+            <ChuckNorrisJoke />
+            <RandomBriyaniPics />
+            <RandomGeekJokes />
+            <RandomActivity />
+            <BreadPuns />
+            <RandomDogimage />
+            <PokeApi />
+            <RandomCreditCard />
+            <RandomFoxImage />
+            <GitHubUser />
+            <RandomCatimages />
+            <ZipCodeDetailsAPI />
+            <WeatherApi />
+            <Covidapi />
+            <CryptoDetails />
+
+        </div>
+    )
 }
 
-export default App
+export default App;
