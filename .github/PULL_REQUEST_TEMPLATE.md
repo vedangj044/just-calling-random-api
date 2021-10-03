a832950c4546b59c4b32e94360a8da19d425fd7d
SobhanDash <sobhan7dash@gmail.com>
2021-10-03 19:17:07 +0530

    Added rick and morty api

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -8,11 +8,11 @@ Optional
 <hr>
 Checklist
 
-- [ ] API used in this PR was not used in just-calling-random-api before.
-- [ ] API used in this PR is Public or I have permission to use it.
-- [ ] My contribution is working.
-- [ ] My contribution doesn't expose any API keys.
-- [ ] My changes don't generate any new warnings.
-- [ ] My changes don't affect any work done by other developers. 
+- [x] API used in this PR was not used in just-calling-random-api before.
+- [x] API used in this PR is Public or I have permission to use it.
+- [x] My contribution is working.
+- [x] My contribution doesn't expose any API keys.
+- [x] My changes don't generate any new warnings.
+- [x] My changes don't affect any work done by other developers. 
 
 To tick a box, replace `[ ]` with `[x]`. 
--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import './App.css'
-import Header from './components/header'
-import Message from './components/content'
-import RandomKanyeQuote from './cards/RandomKanyeQuote'
-import MusicPlayer from './cards/MusicPlayer'
-import RandomQuotes from './cards/randomQuotes'
-import RandomAdvice from './cards/randomAdvice'
-import RandomAnime from './cards/randomAnime'
-import StarWarsAPI from './cards/starWarsAPI'
-import RandomDadJoke from './cards/randomDadJoke'
-import RandomCatFacts from './cards/randomCatFacts'
-import ChuckNorrisJoke from './cards/chuckNorrisJoke'
-import RandomGeekJokes from './cards/randomGeekJokes'
-import RandomBriyaniPics from './cards/RandomBriyaniPics'
-import RandomActivity from './cards/boredAPI'
-import RandomAnimfacts from './cards/randomDogfacts'
-import BreadPuns from './cards/breadPuns'
-import RandomDogimage from './cards/RandomDogimage'
-import PokeApi from './cards/pokeAPI'
-import RandomCreditCard from './cards/randomCreditCard'
-import RandomFoxImage from './cards/randomFoxImage'
-import Covidapi from './cards/covidApi'
-import GitHubUser from './cards/githubUser'
-import RandomCatimages from './cards/RandomCatimages'
-import ZipCodeDetailsAPI from './cards/ZipCodeDetailsAPI'
-import ImageCompression from './cards/imageCompressionAPI'
-import GiphyApi from './cards/giphyapi'
-import Dungeons from './cards/Dungeons'
-import WeatherApi from './cards/weatherApi';
+import "./App.css";
+import Header from "./components/header";
+import Message from "./components/content";
+import RandomKanyeQuote from "./cards/RandomKanyeQuote";
+import MusicPlayer from "./cards/MusicPlayer";
+import RandomQuotes from "./cards/randomQuotes";
+import RandomAdvice from "./cards/randomAdvice";
+import RandomAnime from "./cards/randomAnime";
+import StarWarsAPI from "./cards/starWarsAPI";
+import RandomDadJoke from "./cards/randomDadJoke";
+import RandomCatFacts from "./cards/randomCatFacts";
+import ChuckNorrisJoke from "./cards/chuckNorrisJoke";
+import RandomGeekJokes from "./cards/randomGeekJokes";
+import RandomBriyaniPics from "./cards/RandomBriyaniPics";
+import RandomActivity from "./cards/boredAPI";
+import RandomAnimfacts from "./cards/randomDogfacts";
+import BreadPuns from "./cards/breadPuns";
+import RandomDogimage from "./cards/RandomDogimage";
+import PokeApi from "./cards/pokeAPI";
+import RandomCreditCard from "./cards/randomCreditCard";
+import RandomFoxImage from "./cards/randomFoxImage";
+import Covidapi from "./cards/covidApi";
+import GitHubUser from "./cards/githubUser";
+import RandomCatimages from "./cards/RandomCatimages";
+import ZipCodeDetailsAPI from "./cards/ZipCodeDetailsAPI";
+import ImageCompression from "./cards/imageCompressionAPI";
+import GiphyApi from "./cards/giphyapi";
+import Dungeons from "./cards/Dungeons";
+import WeatherApi from "./cards/weatherApi";
+import RicknMorty from "./cards/ricknmortyApi";
 
 function App() {
   return (
-    <div className='mx-0 xl:mx-40 font-body'>
+    <div className="mx-0 xl:mx-40 font-body">
       <Header />
       <Message />
       <Dungeons />
@@ -59,8 +60,9 @@ function App() {
       <ZipCodeDetailsAPI />
       <WeatherApi />
       <Covidapi />
+      <RicknMorty />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
