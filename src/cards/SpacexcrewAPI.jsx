bfc8b9ed41c459977740051abc6d149271c9e226
yash37158 <yash37158@gmail.com>
2021-10-14 22:45:24 +0530

    small change required

--- src/cards/SpacexcrewAPI.jsx
+++ src/cards/SpacexcrewAPI.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import ContributorInfo from "../components/contributorInfo";
 import ApiInfo from "../components/apiInfo";
-import { data } from "autoprefixer";
 
 const SpacexCrew = () => {
   const [Crew, setCrew] = useState("");
@@ -16,11 +15,8 @@ const SpacexCrew = () => {
     <div className="">
       <div className="mx-4 my-11 rounded-md p-4 border-1 shadow-2xl md:px-4">
         <h1 className="text-xl py-2 text-black Bold">Spacex Crew Members</h1>
-        <img src={Crew.image} style={{ width: "20%" }} alt="Crew Image" />
-        <h1 className="text-2xl font-medium pb-6">
-          {" "}
-          Crew Member : {Crew.name}
-        </h1>
+        <img src={Crew.image} style={{ width: "20%" }} />
+        <h1 className="text-2xl font-medium pb-6">Crew Member : {Crew.name}</h1>
         <h2 className="text-2x; font-medium pb-6">
           Agency name: {Crew.agency}
         </h2>
